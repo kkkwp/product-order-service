4f2e703b7b85837fd16120d5c58cad4ba6cbb045
kkkwp <chxrryda@gmail.com>
2023-07-05 00:55:00 +0900

    🎨 refactor: API 테스트로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -3,11 +3,20 @@ package com.example.productorderservice.product;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.example.productorderservice.ApiTest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
 class ProductApiTest extends ApiTest {
+
+	@Autowired
+	ProductRepository productRepository;
 
 	@Test
 	void 상품등록() {
@@ -27,5 +36,26 @@ class ProductApiTest extends ApiTest {
 
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 		assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+	}
+
+	@Test
+	void 상품수정() {
+		ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+		final long productId = 1L;
+
+		final ExtractableResponse<Response> response = 상품수정요청(productId);
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+		assertThat(productRepository.findById(1L).get().getName()).isEqualTo("상품 수정");
+	}
+
+	private static ExtractableResponse<Response> 상품수정요청(final long productId) {
+		return RestAssured.given().log().all()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(ProductSteps.상품수정요청_생성())
+			.when()
+			.patch("/products/{productId}", productId)
+			.then()
+			.log().all().extract();
 	}
 }
--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -16,8 +16,8 @@ class ProductServiceTest {
 	@Test
 	void 상품수정() {
 		productService.addProduct(ProductSteps.상품등록요청_생성());
-		final Long productId = 1L;
-		final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+		final long productId = 1L;
+		final UpdateProductRequest request = ProductSteps.상품수정요청_생성();
 
 		productService.updateProduct(productId, request);
 
