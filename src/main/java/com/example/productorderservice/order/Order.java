43ef87a3ba4ea110c895a566a27d47245023a0d2
kkkwp <chxrryda@gmail.com>
2023-07-09 23:54:38 +0900

    🎨 refactor: JPA 적용하기

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,26 +1,36 @@
 package com.example.productorderservice.order;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 import org.springframework.util.Assert;
 
 import com.example.productorderservice.product.Product;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private final Product product;
-	private final int quantity;
+	@OneToOne
+	private Product product;
+	private int quantity;
 
 	public Order(final Product product, final int quantity) {
 		Assert.notNull(product, "상품은 필수입니다.");
 		Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
 		this.product = product;
 		this.quantity = quantity;
-	}
-
-	public void assignId(final Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return id;
 	}
 }
